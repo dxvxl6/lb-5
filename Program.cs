bade51bc1e970d09019bf4e1f20ed585f8a4d22e
dxvxl6 <147540545+dxvxl6@users.noreply.github.com>
2024-10-04 09:31:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static DateTime chickenEndTime;
+    static DateTime eggEndTime;
+
+    static void Main()
+    {
+        // Ввод количества повторений пользователем
+        Console.WriteLine("Введите количество повторений:");
+        int repetitions;
+        if (!int.TryParse(Console.ReadLine(), out repetitions) || repetitions <= 0)
+        {
+            Console.WriteLine("Некорректное количество повторений, используем значение по умолчанию 5.");
+            repetitions = 5;
+        }
+
+        // Создание и запуск потоков с передачей количества повторений в методы
+        Thread chickenThread = new Thread(() => SayChicken(repetitions));
+        Thread eggThread = new Thread(() => SayEgg(repetitions));
+
+        chickenThread.Start();
+        eggThread.Start();
+
+        // Ожидание завершения потоков
+        chickenThread.Join();
+        eggThread.Join();
+
+        // Проверка, какой из потоков завершился последним по времени
+        if (chickenEndTime > eggEndTime)
+        {
+            Console.WriteLine("Курица победила в споре.");
+        }
+        else if (eggEndTime > chickenEndTime)
+        {
+            Console.WriteLine("Яйцо победило в споре.");
+        }
+        else
+        {
+            Console.WriteLine("Спор равен.");
+        }
+    }
+
+    static void SayChicken(int repetitions)
+    {
+        for (int i = 0; i < repetitions; i++)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("Курица");
+        }
+        chickenEndTime = DateTime.Now;
+    }
+
+    static void SayEgg(int repetitions)
+    {
+        for (int i = 0; i < repetitions; i++)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("Яйцо");
+        }
+        eggEndTime = DateTime.Now;
+    }
+}
